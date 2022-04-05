a3a9e7701a294b11847d125a3928df6c9d990bbe
vladimirCeli <vladimir.celi@unl.edu.ec>
2022-04-05 17:26:12 -0500

    Lista de  lanzamientos

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+import { Heading, Image} from '@chakra-ui/react'
+
+import { LaunchItem } from './components/LaunchItem'
+import logo from './assets/logo.png'
+import * as API from './services/launches'
+
+export function App () {
+
+  const [launches, setLaunches] = useState([])
+
+  useEffect(() => {
+    API.getAllLaunches().then(setLaunches)
+  }, [])
+
+  return (
+   <>
+    <Image m={4} src={logo} width={300}/>
+     <Heading as='h1' size='lg' m={4}>
+       SpaceX Launches
+       </Heading>
+       <section>
+       {launches.map(launch => (
+         <LaunchItem key={launch.flight_number} {...launch}/>
+       ))}
+       </section>
+   </>
+  )
+}
